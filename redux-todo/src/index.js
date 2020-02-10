b32b099acccf0596301cb9d30cbcd38c1968f3eb
JenniKate Wallace <jennikate@gmail.com>
2020-02-10 09:56:55 +0000

    connecting redux to react

--- redux-todo/src/index.js
+++ redux-todo/src/index.js
@@ -1,1 +1,14 @@
-import index from './js/index';
+import React from 'react';
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import store from './js/store/index'
+import App from './js/components/App'
+
+
+//Provider wraps up your React application and makes it aware of the entire Redux's store
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
